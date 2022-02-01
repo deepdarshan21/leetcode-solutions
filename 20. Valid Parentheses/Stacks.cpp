ced0ba77f1fbf4b0b5fb3ddf460f68194e20e379
Deepdarshan <deepdarshan21@gmail.com>
2022-02-01 20:39:17 +0530

    Time Complexity: O(n) Space: O(1)

---
+++ 20. Valid Parentheses/Stacks.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Approach: Using Stacks
+       Stacks helps me in finding what kind of bracket is lastly used
+
+   Edge Cases:
+       1. At last store stack has some opening brackets remaining
+       2. String has only closing bracket
+
+   Time Complexity: O()
+   Space: O()
+*/
+
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> store;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                store.push(s[i]);
+            } else if (s[i] == ')') {
+                if (!store.empty() && store.top() == '(') {
+                    store.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == ']') {
+                if (!store.empty() && store.top() == '[') {
+                    store.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == '}') {
+                if (!store.empty() && store.top() == '{') {
+                    store.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (!store.empty()) {
+            return false;
+        }
+        return true;
+    }
+};
