5c3e1e8a334da41655fd648a368d85aa59ac0c16
Deepdarshan <deepdarshan21@gmail.com>
2022-02-02 12:48:50 +0530

    Time Complexity: O(n) Space: O(1)

---
+++ 1. Two Sum/Two Pointer.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Approach: Two Pointer
+       
+
+   Edge Cases:
+       
+
+   Time Complexity: O(n) Space: O(1)
+*/
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        int i = 0, j = nums.size() - 1;
+        while (i < j) {
+            if ((nums[i] + nums[j]) == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+            if ((nums[i] + nums[j]) > target) {
+                j--;
+            }
+            if ((nums[i] + nums[j]) < target) {
+                i++;
+            }
+        }
+        return ans;
+    }
+};
