b26da589b01baca2c0ed0e4af63f96331a064662
Deepdarshan <deepdarshan21@gmail.com>
2022-02-13 15:12:33 +0530

    Time Complexity: O(m+n) Space: O(1)

---
+++ 21. Merge Two Sorted Lists/Optimized Recursion.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/*
+   Approach:
+        Connect correct links and make one linked list
+
+   Edge Cases:
+        1. Any list has size zero
+
+   Time Complexity: O(m+n) Space: O(1)
+   Status: Accepted
+   Runtime: 8 ms faster than: 75.87%
+   Memory Usage: 14.8 MB less than: 45.16%
+*/
+
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) {
+            return list2;
+        }
+        if (list2 == nullptr) {
+            return list1;
+        }
+        if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
