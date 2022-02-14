db6d61de22f8fffb36d75fea59a149a3be3b8b9e
Deepdarshan <deepdarshan21@gmail.com>
2022-02-15 04:34:12 +0530

    Time Complexity: O(n) Space: O(1)

---
+++ 26. Remove Duplicates from Sorted Array/Naive.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Brute Force
+
+
+   Edge Cases:
+
+
+   Time Complexity: O() Space: O()
+   Status: Accepted
+   Runtime: 608 ms faster than: 5.01%
+   Memory Usage: 18.5 MB less than: %
+*/
+
+class Solution {
+   public:
+    int removeDuplicates(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
