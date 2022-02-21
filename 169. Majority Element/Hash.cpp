a18b7c5865faeecde4a61eec05d0b8a175768ddb
Deepdarshan <deepdarshan21@gmail.com>
2022-02-21 23:39:55 +0530

    Time Complexity: O(n) Space: O(n)

---
+++ 169. Majority Element/Hash.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Hashing
+
+
+   Edge Cases:
+
+   Time Complexity: O(n) Space: O(n)
+   Status: Accepted
+   Runtime: 46 ms faster than: 8.68%
+   Memory Usage: 19.6 MB less than: 52.27%
+   Remarks:
+*/
+
+class Solution {
+   public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        for (int i = 0; i < nums.size(); i++) {
+            counts[nums[i]]++;
+        }
+        int ans;
+        for (auto i : counts) {
+            if (i.second > nums.size() / 2) {
+                ans = i.first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
