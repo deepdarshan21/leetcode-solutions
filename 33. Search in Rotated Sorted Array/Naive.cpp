2e4b450d5a56e858cf3aab313fedef3a8bf0b4a9
Deepdarshan <deepdarshan21@gmail.com>
2022-02-22 15:37:10 +0530

    Time Complexity: O(n) Space: O(1)

---
+++ 33. Search in Rotated Sorted Array/Naive.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Linear Search
+
+
+   Edge Cases:
+
+   Time Complexity: O(n) Space: O(1)
+   Status: Accepted
+   Runtime: 13 ms faster than: 8.00%
+   Memory Usage: 11.1 MB less than: 30.00%
+   Remarks: It was running in O(n) time complexity but question said that solve it in O(logn) time complexity
+*/
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
