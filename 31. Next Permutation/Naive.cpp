a4a1c67932fdabce8e9f052cddc51d1fbd61a1f5
Deepdarshan <deepdarshan21@gmail.com>
2022-02-22 13:05:42 +0530

    Time Limit Exceeded

---
+++ 31. Next Permutation/Naive.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Brute Force
+        First store all the possible permutation in circular array from the set of elements present in given vector(permutation),
+        then find the position of given vector(permutation), therfore next permutation after that permutation
+        is next element from the store.
+
+   Edge Cases:
+
+   Time Complexity: O(n!) Space: O(n!)
+   Status: Time Limit Exceeded
+   Runtime:  ms faster than: %
+   Memory Usage:  MB less than: %
+   Remarks: Because of constraints, this approach will result in TLE
+*/
+
+class Solution {
+   public:
+};
