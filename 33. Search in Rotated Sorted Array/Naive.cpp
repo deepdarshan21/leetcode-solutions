6f7ac82c9a706508a51d696eec09ec1f7e839c3b
Deepdarshan <deepdarshan21@gmail.com>
2022-02-22 15:38:40 +0530

    Time Complexity: O(nlogn) Space: O(1)

--- 33. Search in Rotated Sorted Array/Naive.cpp
+++ 33. Search in Rotated Sorted Array/Naive.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
    Edge Cases:
 
-   Time Complexity: O(n) Space: O(1)
+   Time Complexity: O(nlogn) Space: O(1)
    Status: Accepted
    Runtime: 13 ms faster than: 8.00%
    Memory Usage: 11.1 MB less than: 30.00%
