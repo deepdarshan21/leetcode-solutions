be2ad5a37d3042a7b9a030e805fa3a8cc773e2aa
Deepdarshan <deepdarshan21@gmail.com>
2022-02-27 05:45:50 +0530

    Time Complexity: O(n) Space: O(1)

--- 53. Maximum Subarray/Kadane Algo.cpp
+++ 53. Maximum Subarray/Kadane Algo.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 /*
-   Approach:
+   Approach: Kadane's Algo
+        Initialize:
+            max_so_far = INT_MIN
+            max_ending_here = 0
 
+        Loop for each element of the array
+            (a) max_ending_here = max_ending_here + a[i]
+            (b) if(max_so_far < max_ending_here)
+                max_so_far = max_ending_here
+            (c) if(max_ending_here < 0)
+                max_ending_here = 0
+        return max_so_far
 
    Edge Cases:
 
