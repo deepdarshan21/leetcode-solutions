99b6372dda844bd8d788c62037feec50539c3bb3
Deepdarshan <deepdarshan21@gmail.com>
2022-02-28 12:05:43 +0530

    Time Complexity: O(n) Space: O(n)

--- 228. Summary Ranges/Naive.cpp
+++ 228. Summary Ranges/Naive.cpp
@@ -3,8 +3,21 @@ using namespace std;
 
 /*
    Approach: Linear Search
+        Use two variable "start" and "end" to store start and end of the range
+        Intially store first element of "nums" in "start"
+        Iterate on "nums" vector form element one
+            if "current element of nums" is just greater that by "1" to "previous element of nums":
+                    "end" equal to "current element of nums"
+            else:
+                    if "end" is greater than "start":
+                            Store this range as '"start"->"end"'
+                    else:
+                            Store this range as '"start"'
+                    "start" equal to "current element of nums"
 
    Edge Cases:
+        1. Subtraction of two number may cause int overflow.
+            use,  "nums[i] == (nums[i - 1] + 1)" to restrict it.
 
    Time Complexity: O(n) Space: O(n)
    Status: Accepted
