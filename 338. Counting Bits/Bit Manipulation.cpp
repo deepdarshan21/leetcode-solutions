ca9f211aa3f9861df74d0112a90290bfe22af220
Deepdarshan <deepdarshan21@gmail.com>
2022-03-01 20:26:21 +0530

    Time Complexity: O(nlogn) Space: O(1)

--- 338. Counting Bits/Bit Manipulation.cpp
+++ 338. Counting Bits/Bit Manipulation.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 /*
-   Approach:
-
+   Approach: Bit Manipulation
+        "&"-> Binary AND operator
+        if n-> xxx1000...
+        then n-1 -> xxx0111...
+        so, binary AND of n and n-1 (n & (n-1)) is xxx0000
+        Repeating this thing will give total number of "1" in given binary number
 
    Edge Cases:
 
-   Time Complexity: O() Space: O()
-   Status:
-   Runtime:  ms faster than: %
-   Memory Usage:  MB less than: %
+   Time Complexity: O(nlogn) Space: O(1)
+   Status: Accepted
+   Runtime: 3 ms faster than: 95.58%
+   Memory Usage: 8.4 MB less than: 34.00%
    Remarks:
 */
 
