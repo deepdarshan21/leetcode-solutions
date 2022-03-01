e2cd8e119d05fc466d525fa44100c6a455758afb
Deepdarshan <deepdarshan21@gmail.com>
2022-03-01 21:10:32 +0530

    Time Complexity: O(n) Space: O(1)

--- 338. Counting Bits/Bit Manipulation II.cpp
+++ 338. Counting Bits/Bit Manipulation II.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 /*
    Approach: Bit Manipulation
-
+        If x/2=y => number of 1's in "x" - number of 1's in "y" can be (0 or 1).
+        ~ if x is even => number of 1's in x - number of 1's in y is 0(if y is xx1xx then x is xx1xx0)
+        ~ if x is odd => number of 1's in x - number of 1's in y is 1(if y is xx1xx0 then x is xx1xxx1)
 
    Edge Cases:
 
