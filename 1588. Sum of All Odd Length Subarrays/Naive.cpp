aecc2c3531de2ea90be875f51ef0f05cdb7dee31
Deepdarshan <deepdarshan21@gmail.com>
2022-03-03 14:44:24 +0530

    Time Complexity: O(n^2) Space: O(1)

---
+++ 1588. Sum of All Odd Length Subarrays/Naive.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Brute Force
+        Find sum of every possible subarray and add them to give answer.
+
+   Edge Cases:
+
+   Time Complexity: O(n^2) Space: O(1)
+   Status: Accepted
+   Runtime: 20 ms faster than: 8.98%
+   Memory Usage: 8.3 MB less than: 81.05%
+   Remarks:
+*/
+
+class Solution {
+   public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum = 0, size;
+        for (int i = 0; i < arr.size(); i++) {
+            size = 1;
+            while (i + size <= arr.size()) {
+                sum += accumulate(arr.begin() + i, arr.begin() + i + size, 0);
+                size += 2;
+            }
+        }
+        return sum;
+    }
+};
