a29456d3c6a5b4b34a0746a0c11f810ca7a38816
Deepdarshan <deepdarshan21@gmail.com>
2022-03-04 23:22:43 +0530

    Time Complexity: O(n) Space: O(1)

---
+++ 1572. Matrix Diagonal Sum/Naive.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach:
+        Sum the elements of both the digonals and if the size of given matrix is odd then we added
+        the middle element twice, so remove that element at last
+
+   Edge Cases:
+
+   Time Complexity: O() Space: O()
+   Status:
+   Runtime:  ms faster than: %
+   Memory Usage:  MB less than: %
+   Remarks:
+*/
+
+class Solution {
+   public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            sum += mat[i][i] + mat[i][mat.size() - 1 - i];
+        }
+        if (mat.size() % 2 == 1) {
+            sum -= mat[mat.size() / 2][mat.size() / 2];
+        }
+        return sum;
+    }
+};
