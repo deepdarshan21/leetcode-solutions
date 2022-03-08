69488e597069cc3e0684e5556655a28edc7b2dc5
Deepdarshan <deepdarshan21@gmail.com>
2022-03-09 00:03:22 +0530

    Time Complexity: O(n) Space: O(n)

--- 141. Linked List Cycle/Naive.cpp
+++ 141. Linked List Cycle/Naive.cpp
@@ -15,7 +15,7 @@ struct ListNode {
    Edge Cases:
 
    Time Complexity: O(n) Space: O(n)
-   Status: Wrong answer
+   Status: Accepted
    Runtime: 255 ms faster than: 5.27%
    Memory Usage: 8.9 MB less than: 16.03%
    Remarks:
