e104f40ad42a3c364b75d694bb65880739147862
Deepdarshan <deepdarshan21@gmail.com>
2022-03-19 15:16:51 +0530

    Time Complexity: O(nlogn) Space: O(n^2)

---
+++ 56. Merge Intervals/Sorting.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Sorting
+        Sort the array according to start of every interval
+        Then make interval accordingly
+
+   Edge Cases:
+
+   Time Complexity: O(nlogn) Space: O(n^2)
+   Status: Accepted
+   Runtime: 76 ms faster than: 23.16%
+   Memory Usage: 19.9 MB less than: 32.89%
+   Remarks:
+*/
+
+class Solution {
+   public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for (auto interval : intervals) {
+            if (interval[0] > res[res.size() - 1][1]) {
+                res.push_back(interval);
+            } else {
+                res[res.size() - 1][1] = max(interval[1], res[res.size() - 1][1]);
+            }
+        }
+        return res;
+    }
+};
