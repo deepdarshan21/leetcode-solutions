fe892c073814f4df99614b055023d9cad63399bc
Deepdarshan <deepdarshan21@gmail.com>
2022-03-29 01:10:04 +0530

    Time Complexity: O(logn) Space: O(1)

--- 81. Search in Rotated Sorted Array II/Binary Search.cpp
+++ 81. Search in Rotated Sorted Array II/Binary Search.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 /*
-   Approach:
+   Approach: Binary Search
+        try to find the correct part where element can exist.
 
+   Edge Cases: if mid element, low element and high element have same value
 
-   Edge Cases:
-
-   Time Complexity: O() Space: O()
-   Status:
-   Runtime:  ms faster than: %
-   Memory Usage:  MB less than: %
+   Time Complexity: O(logn) Space: O(1)
+   Status: Accepted
+   Runtime: 19 ms faster than: 6.56%
+   Memory Usage: 13.8 MB less than: 99.50%
    Remarks:
 */
 
@@ -26,10 +26,22 @@ class Solution {
                 if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
                     low++;
                     high--;
-                } else if (nums[low] <= target && nums[mid] > target) {
-                    high = mid - 1;
                 } else {
-                    low = mid + 1;
+                    if (nums[low] <= nums[mid]) {
+                        // low to mid is sorted
+                        if (nums[low] <= target && nums[mid] > target) {
+                            high = mid - 1;
+                        } else {
+                            low = mid + 1;
+                        }
+                    } else {
+                        // mid to high is sorted
+                        if (nums[mid] < target && nums[high] >= target) {
+                            low = mid + 1;
+                        } else {
+                            high = mid - 1;
+                        }
+                    }
                 }
             }
         }
