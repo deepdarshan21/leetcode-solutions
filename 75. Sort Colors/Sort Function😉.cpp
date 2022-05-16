2e3b3bce5188fa260fc985fa408abaeaf5df6ff9
Deepdarshan <deepdarshan21@gmail.com>
2022-05-16 12:10:12 +0530

    Time Complexity: O(nlogn) Space: O(1)

---
+++ 75. Sort Colors/Sort Function😉.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Sorting😉😎
+
+
+   Edge Cases:
+
+   Time Complexity: O(nlogn) Space: O(1)
+   Status: Accepted
+   Runtime: 0 ms faster than: 100%
+   Memory Usage: 8.3 MB less than: 25.30%
+   Remarks:
+*/
+
+class Solution {
+   public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
