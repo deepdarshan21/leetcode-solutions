bdb63dc02fe1e5aadd4c474b3c5ba1f7013e3b03
Deepdarshan <deepdarshan21@gmail.com>
2022-11-23 23:12:30 +0530

    Time Limit Exceeded

---
+++ 53. Maximum Subarray/Divide and Conqure.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+   Approach: Divide and Conqure
+
+   Edge Cases:
+
+   Time Complexity: O() Space: O()
+   Status: Time Limit Exceeded
+   Runtime:  ms faster than: %
+   Memory Usage:  MB less than: %
+   Remarks:
+*/
+
+class Solution {
+   public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        return help(nums, 0, n - 1);
+    }
+    int help(vector<int>& nums, int start, int end) {
+        if (end <= start) return nums[start];
+        int mid = start + (end - start) / 2;
+        int sum, maxSum1 = 0, maxSum2 = 0;
+        sum = 0;
+        for (int i = mid; i >= start; i--) {
+            sum += nums[i];
+            maxSum1 = max(maxSum1, sum);
+        }
+        int sum = 0;
+        for (int i = mid + 1; i <= end; i++) {
+            sum += nums[i];
+            maxSum2 = max(sum, maxSum2);
+        }
+        int temp1 = help(nums, 0, mid);
+        int temp2 = help(nums, mid + 1, end);
+        return max(maxSum1 + maxSum2, max(temp1, temp2));
+    }
+};
